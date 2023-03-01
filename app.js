62145d7062ed639a14de906c4b5672a2fa025d42
Ovanesyanaz <Ovanesyanaz@mail.ru>
2023-03-01 18:12:25 +0300

    bug fix related to page refresh

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ app.use("/server", require("./routes/zapros.js"))
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,'client', 'build')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
-    });
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 const PORT = 3000
 
 async function start() {
